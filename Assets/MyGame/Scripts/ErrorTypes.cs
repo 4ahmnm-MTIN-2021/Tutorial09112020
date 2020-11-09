f1c8969a0aed6183ef32bebda5504cfce9db5026
smeerws <hello@smeerws.at>
2020-11-09 22:09:05 +0100

    methoden deklaration und aufruf

---
+++ Assets/MyGame/Scripts/ErrorTypes.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ErrorTypes : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        MyMethode();
+        Debug.Log(MyMethodeAdd() + MyMethodeAdd());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //MyMethode();
+    }
+
+    void MyMethode()
+    {
+        Debug.Log("Hello Joe");
+    }
+
+    int MyMethodeAdd()
+    {
+        return 5;
+    }
+
+    string MyMethodeString()
+    {
+        return "";
+    }
+}
